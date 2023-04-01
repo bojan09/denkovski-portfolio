73f341f29346685d2a26b4b44e7790b3fb99afb6
Bojan <bojan-stanimirovski097@hotmail.com>
2023-04-01 16:33:01 +0200

    added about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import "./About.css";
+
+import { Link } from "react-router-dom";
+
+import facebook from "../../assets/socials/facebook.svg";
+import instagram from "../../assets/socials/instagram.svg";
+import linkedin from "../../assets/socials/linkedin.svg";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <h2 className="about-heading">About me</h2>
+
+      {/* Social Icons */}
+      <div className="about-social_icons">
+        <Link to="https://www.facebook.com/milan.denkovski" target="_blank">
+          <img src={facebook} alt="facebook" />
+        </Link>
+        <Link to="https://www.instagram.com/milan_denkovski95/" target="_blank">
+          <img src={instagram} alt="instagram" />
+        </Link>
+        <Link
+          to="https://www.linkedin.com/in/milan-denkovski-711104267/"
+          target="_blank"
+        >
+          <img src={linkedin} alt="linkedin" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default About;
