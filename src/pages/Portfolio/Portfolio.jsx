113542bc3219c90f799d9af27082ebe619dcb7ee
Bojan <bojan-stanimirovski097@hotmail.com>
2023-04-07 20:10:48 +0200

    fixing portfolio grid

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -6,29 +6,41 @@ import wineLandingPage from "../../assets/projects/Home Page.png";
 import herbalMobilePage from "../../assets/projects/Naslovna.png";
 import threeRedWinesPage from "../../assets/projects/Red.png";
 import breadAndButterWinePage from "../../assets/projects/Brad  Butter.png";
+import threeWines from "../../assets/projects/three-wines.png";
 
 const Portfolio = () => {
   return (
     <div className="portfolio">
       <h1 className="portfolio-heading">My Portfolio</h1>
 
+      <h1 className="portfolio-grid_heading">Web & Mobile</h1>
       <div className="portfolio-grid">
-        <h1 className="portfolio-grid_heading">Web & Mobile</h1>
+        <div className="portfolio-grid_left">
+          <div className="portfolio-grid_image">
+            <img src={wineLandingPage} alt="wineLandingPage" />
+          </div>
 
-        <div className="portfolio-grid_image">
-          <img src={wineLandingPage} alt="wineLandingPage" />
+          <div className="portfolio-grid_image">
+            <img src={herbalMobilePage} alt="herbalMobilePage" />
+          </div>
         </div>
 
-        <div className="portfolio-grid_image">
-          <img src={herbalMobilePage} alt="herbalMobilePage" />
-        </div>
+        <div className="portfolio-grid_right">
+          <div className="portfolio-grid_image">
+            <img
+              src={threeWines}
+              alt="threeRedWinesPage"
+              className="three-red_wines"
+            />
+          </div>
 
-        <div className="portfolio-grid_image">
-          <img src={threeRedWinesPage} alt="threeRedWinesPage" />
-        </div>
+          <div className="portfolio-grid_image">
+            <img src={threeRedWinesPage} alt="threeRedWinesPage" />
+          </div>
 
-        <div className="portfolio-grid_image">
-          <img src={breadAndButterWinePage} alt="breadAndButterWinePage" />
+          <div className="portfolio-grid_image">
+            <img src={breadAndButterWinePage} alt="breadAndButterWinePage" />
+          </div>
         </div>
       </div>
     </div>
