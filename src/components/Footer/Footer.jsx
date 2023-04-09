7573b8d254430c154373995c8961349c0882b9a8
Bojan <bojan-stanimirovski097@hotmail.com>
2023-04-09 11:36:25 +0200

    Footer, added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./Footer.css";
+
+import locationIcon from "../../assets/icons/pin-destination.svg";
+import phoneIcon from "../../assets/icons/mobile.svg";
+import MailIcon from "../../assets/icons/envelope.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <h2 className="footer-heading">Contact me</h2>
+      <div className="footer-icons_container">
+        <div className="footer-location_icon">
+          <img src={locationIcon} alt="location_icon" />
+          <p className="footer-paragraph">St. Leskovacka</p>
+        </div>
+        <div className="footer-phone_icon">
+          <img src={phoneIcon} alt="phone_icon" />
+          <p className="footer-paragraph">+ 389 78 457 695</p>
+        </div>
+        <div className="footer-email_icon">
+          <img src={MailIcon} alt="mail_icon" />
+          <p className="footer-paragraph">milan.denkovski@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
